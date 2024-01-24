b407df928119c5bec2efc4bd3b0dd08794c2033d
Hannah Kendall <hbkendall@outlook.com>
2024-01-23 17:05:34 -0800

    Add Bootstrap Navbar, start Home page

---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Almost - The Band</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Gallery</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
